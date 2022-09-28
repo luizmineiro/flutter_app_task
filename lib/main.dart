d01b09ccc733af6611919e00128de1725ac20286
luizmineiro <jluizmineiro@gmail.com>
2022-09-28 17:13:10 -0300

    erro na imagem

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:alura_flutter_curso_1/screens/form_screen.dart';
 import 'package:alura_flutter_curso_1/screens/tela_inicial.dart';
 import 'package:flutter/material.dart';
 
@@ -22,7 +23,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: InitialScreen(),
+      home: FormScreen(),
     );
   }
 }
---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({super.key});
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nova Tarefa'),
+      ),
+      body: Center(
+        child: Container(
+          height: 650,
+          width: 375,
+          decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 2),
+          ),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: nameController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Nome',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: difficultyController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Dificuldade',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    onChanged: (text) {
+                      setState(() {});
+                    },
+                    controller: imageController,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Imagem',
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(imageController.text, fit: BoxFit.cover,),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print(nameController.text);
+                    print(int.parse(difficultyController.text));
+                    print(imageController.text);
+                  },
+                  child: Text('Adicionar'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
