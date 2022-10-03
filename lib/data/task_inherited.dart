bffa35f5a088428458b240409a5da9af40574452
luizmineiro <jluizmineiro@gmail.com>
2022-10-03 17:44:53 -0300

    update

--- lib/data/task_inherited.dart
+++ lib/data/task_inherited.dart
@@ -5,11 +5,11 @@ class TaskInherited extends InheritedWidget {
   TaskInherited({super.key, required Widget child}) : super(child: child);
 
   final List<Tasks> taskList = [
-    Tasks('Estudar Flutter', 'assets/images/flutter.png', 3),
-    Tasks('Andar de Bike', 'assets/images/bike.webp', 2),
-    Tasks('Ler 50 páginas', 'assets/images/ler.jpg', 1),
-    Tasks('Meditar', 'assets/images/meditar.jpeg', 4),
-    Tasks('Jogar', 'assets/images/jogar.jpg', 3),
+    const Tasks('Estudar Flutter', 'assets/images/flutter.png', 3),
+    const Tasks('Andar de Bike', 'assets/images/bike.webp', 2),
+    const Tasks('Ler 50 páginas', 'assets/images/ler.jpg', 1),
+    const Tasks('Meditar', 'assets/images/meditar.jpeg', 4),
+    const Tasks('Jogar', 'assets/images/jogar.jpg', 3),
   ];
 
   void newTask(String name, String photo, int difficulty) {
