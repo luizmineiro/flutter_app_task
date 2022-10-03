79a98dd8d2d47776c1a18672d4ab08709d3968ea
luizmineiro <jluizmineiro@gmail.com>
2022-10-03 14:02:48 -0300

    update

--- lib/data/task_inherited.dart
+++ lib/data/task_inherited.dart
@@ -22,6 +22,6 @@ class TaskInherited extends InheritedWidget {
 
   @override
   bool updateShouldNotify(TaskInherited oldWidget) {
-    return true;
+    return oldWidget.taskList.length != taskList.length;
   }
 }
